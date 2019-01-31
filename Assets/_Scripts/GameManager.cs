001a4073ef2d2684fb9f52d9df5473db40a0fb8b
David <davidlafantaisie@outlook.com>
2019-01-31 18:24:44 -0500

    Added dish loading in game manager. Dishes should be set with tag "Dish". Game manager will find them all in initGame() and then assign them to an array.

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
+    //-------------------------------------------------------//
+    //---------------------- VARIABLES ----------------------//
+    //-------------------------------------------------------//
+
+    //Public
+    public enum difficulty { EASY, MEDIUM, HARD }
     public static GameManager instance = null;
+    public GameObject[] dishes;
+    public List<DishScript> dishScripts;
+
+    //Private
+    private int stage = 1;
+    private int lowStage = 1;
+    private int highStage = 3;
+    private difficulty mode = difficulty.EASY;
+
+    //----------------------------------------------------------//
+    //---------------------- INITIALIZERS ----------------------//
+    //----------------------------------------------------------//
 
     private void Awake()
     {
@@ -14,6 +32,52 @@ public class GameManager : MonoBehaviour {
             Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        initGame();
+    }
+
+    void initGame()
+    {
+        setStage(1);
+        setDifficulty(difficulty.EASY);
+        dishes = GameObject.FindGameObjectsWithTag("Dish");
+        dishScripts.Clear();
+        for(int i = 0; i < dishes.Length; i++)
+        {
+            dishScripts.Add(dishes[i].GetComponent<DishScript>());
+        }
+    }
+
+    //-----------------------------------------------------//
+    //---------------------- SETTERS ----------------------//
+    //-----------------------------------------------------//
+
+    void setDifficulty(difficulty m)
+    {
+        mode = m;
+    }
+
+    void setStage(int s)
+    {
+        if (s <= highStage && s >= lowStage)
+            stage = s;
+    }
+
+    //-----------------------------------------------------//
+    //---------------------- GETTERS ----------------------//
+    //-----------------------------------------------------//
+
+    difficulty getDifficulty()
+    {
+        return mode;
+    }
+
+    int getStage()
+    {
+        return stage;
     }
 
 }

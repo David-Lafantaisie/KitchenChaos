e1477db23ac835e1302879031a28a883cac91ce5
StellarSpirit <34275038+StellarSpirit@users.noreply.github.com>
2019-03-11 22:03:15 -0400

    Developed judge system
    Not ready yet

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -21,6 +21,9 @@ public class GameManager : MonoBehaviour {
     public GameObject[] dishSpawns;
     public List<GameObject> dishes;
     public List<BurgerDishScript> dishScripts;
+	public List<GameObject> judges;
+	public List<Judge> judgeScripts;
+	public float score;
 
 
 
@@ -30,6 +33,7 @@ public class GameManager : MonoBehaviour {
     private int lowStage = 1;
     private int highStage = 3;
     private int dishListLength;
+	private int numJudges = 0; //THIS ISNT BEING USED RN, USE FOR IF/WHEN WE INCORPORATE MULTIPLE JUDGES AT ONCE
     private difficulty mode = difficulty.EASY;
 
     //----------------------------------------------------------//
@@ -50,12 +54,19 @@ public class GameManager : MonoBehaviour {
     {
         initGame();
     }
-
-    void initGame()
+	    void initGame()
     {
         setStage(1);
         setDifficulty(difficulty.HARD);
         dishSpawns = GameObject.FindGameObjectsWithTag("DishSpawn");
+		GameObject[] judgesTemp = GameObject.FindGameObjectsWithTag ("Judge");
+		int z = Random.Range (0, judgesTemp.Length - 1); //I COULDNT THINK OF A GOOD NAME FOR THIS INT, FEEL FREE TO CHANGE
+		judges.Add (judgesTemp[z]); //WHEN WE HAVE MULTIPLE JUDGES, MOVE ALL THIS STUFF INTO THE FOR LOOP BELOW
+		judgeScripts.Add(judges[0].GetComponent<Judge>());
+		judgeScripts [0].SetActive (true);
+		Debug.Log (judges[0] + "SET AS ACTIVE JUDGE");
+		numJudges++;
+
         for (int i = 0; i < (int)mode; i++)
         {
             dishes.Add(Instantiate(dish, dishSpawns[i].transform.position, dishSpawns[i].transform.rotation));
@@ -63,6 +74,21 @@ public class GameManager : MonoBehaviour {
         }
         dishListLength = (int)mode;
     }
+
+	void Update() //JUST USING THIS TO TEST THE JUDGE SYSTEM
+	{
+		if (Input.GetKeyDown (KeyCode.X)) {
+			judgeBurgers ();
+		}
+	}
+
+	void judgeBurgers()
+	{
+		for (int i = 0; i < dishListLength; i++) {
+			score += judgeScripts [0].JudgeBurger (dishScripts[i]);
+			Debug.Log ("FINAL SCORE = " + score);
+		}
+	}
 
     //-----------------------------------------------------//
     //---------------------- SETTERS ----------------------//
@@ -108,5 +134,6 @@ public enum BurgerIngType //add the rest later as more ingredients are added to 
 	LETTUCE,
 	TOMATO,
 	ONION,
-	CHEESE
+	CHEESE,
+	TOPBUN
 }
--- Assets/_Scripts/Judge.cs
+++ Assets/_Scripts/Judge.cs
@@ -6,6 +6,8 @@ public class Judge : MonoBehaviour {
 	[SerializeField] int[] BurgerIngValue; //CHANGE THE SIZE OF THIS IN THE PREFAB IF THE ENUMERATION FOR INGREDIENTS IS RESIZED
 	[SerializeField] bool PickyAboutCookTime;
 
+	private bool active = false;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -16,24 +18,47 @@ public class Judge : MonoBehaviour {
 		
 	}
 
-	float JudgeBurger(BurgerDishScript burger)
+	public float JudgeBurger(BurgerDishScript burger)
 	{
 		float cumulativeScore = 0;
 		float returnScore = 0;
 		int numIngredients = 0;
 		int cookTimeModifier = 0;
-		for (int i = 0; i < burger.getIngListLength (); i++) {
-			BurgerIngredientScript testIngredient = burger.ingredientScripts[i];
-			cumulativeScore += BurgerIngValue[(int)testIngredient.getIngredientType()];
-			if (testIngredient.getIngredientType () != BurgerIngType.BUN) {
-				numIngredients++;
+		if (burger.getIngListLength () != 0) {
+			for (int i = 0; i < burger.getIngListLength (); i++) {
+				BurgerIngredientScript testIngredient = burger.ingredientScripts [i];
+				cumulativeScore += BurgerIngValue [(int)testIngredient.getIngredientType ()];
+				if (testIngredient.getIngredientType () == BurgerIngType.BUN && i != 0 ||
+				    testIngredient.getIngredientType () == BurgerIngType.TOPBUN && i != burger.getIngListLength () - 1) {
+					cumulativeScore -= 2;
+				} else if (testIngredient.getIngredientType () != BurgerIngType.BUN && testIngredient.getIngredientType () != BurgerIngType.TOPBUN) {
+					numIngredients++;
+					if (testIngredient.getIngredientType () == BurgerIngType.HAMBURGER) {
+						if (i != 1) {
+							cumulativeScore -= 1;
+						}
+						if (PickyAboutCookTime) {
+							cookTimeModifier = testIngredient.getCookScore ();
+						}
+					}
+				}
+
 			}
-			if (testIngredient.getIngredientType () == BurgerIngType.HAMBURGER && PickyAboutCookTime) {
-				cookTimeModifier = testIngredient.getCookScore ();
-			}
-		}
-		returnScore = cumulativeScore / (float)numIngredients;
-		returnScore = Mathf.Clamp (returnScore + (float)cookTimeModifier, 0.0f, 10.0f);
-		return returnScore;
+			returnScore = cumulativeScore / (float)numIngredients;
+			returnScore = Mathf.Clamp (returnScore + (float)cookTimeModifier, 0.0f, 10.0f);
+			Debug.Log (this.gameObject + " RANKS THE BURGER " + returnScore + "POINTS.");
+			return returnScore;
+		} else
+			return 0;
+	}
+
+	public void SetActive(bool target)
+	{
+		active = target;
+	}
+
+	public bool IsActive()
+	{
+		return active;
 	}
 }
